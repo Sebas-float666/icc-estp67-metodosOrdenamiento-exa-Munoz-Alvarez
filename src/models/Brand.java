22f16f7d2130de62f348d5e966d10e9924e8b13a
Your Name <you@example.com>
2026-05-20 14:00:49 +0000

    UPD3

--- src/models/Brand.java
+++ src/models/Brand.java
@@ -3,39 +3,45 @@ package models;
 import java.util.Arrays;
 
 public class Brand {
-private String brandName;
-private CarModel[] models;
-public Brand(String brandName, CarModel[] models) {
-  this.brandName = brandName;
-  this.models = models;
-}
-public String getBrandName() {
-  return brandName;
-}
-public void setBrandName(String brandName) {
-  this.brandName = brandName;
-}
-public CarModel[] getModels() {
-  return models;
-}
-public void setModels(CarModel[] models) {
-  this.models = models;
-}
+  private String brandName;
+  private CarModel[] models;
 
-public int getTotalValidYears(){
-  int total=0;
-  for (CarModel model : models){
-    for (CarYear year : model.getYears()){
-      if (year.isValid()){
-        total ++;
+  public Brand(String brandName, CarModel[] models) {
+    this.brandName = brandName;
+    this.models = models;
+  }
+
+  public String getBrandName() {
+    return brandName;
+  }
+
+  public void setBrandName(String brandName) {
+    this.brandName = brandName;
+  }
+
+  public CarModel[] getModels() {
+    return models;
+  }
+
+  public void setModels(CarModel[] models) {
+    this.models = models;
+  }
+
+  public int getTotalValidYears() {
+    int total = 0;
+    for (CarModel model : models) {
+      for (CarYear year : model.getYears()) {
+        if (year.isValid()) {
+          total++;
+        }
       }
     }
+    return total;
   }
-  return total;
-}
-@Override
-public String toString() {
-  return "Brand [brandName=" + brandName + ", models=" + Arrays.toString(models) + "]";
-}
+
+  @Override
+  public String toString() {
+    return "Brand [brandName=" + brandName + ", models=" + Arrays.toString(models) + "]";
+  }
 
 }
